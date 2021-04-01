4a4c5275e33e47612a03f27cdef077bcdaffdb3b
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-01 15:05:46 +0530

    Added:Functionality to validate First name

--- src/main/java/com/Bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/Bridgelabz/UserRegistrationMain.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class UserRegistrationMain {
     public static void main(String[] args) {
-
+        int choice;
+        Scanner scanner=new Scanner(System.in);
+        UserRegistrationOperations userRegistrationOperations=new UserRegistrationOperations();
+        System.out.println("**** User Registration Checking System ****");
+        do {
+            System.out.println("\n"+"1:Validate first name" + "\n" + "Enter your choice:");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter your first name");
+                    String firstName = scanner.next();
+                    userRegistrationOperations.validateFirstName(firstName);
+                    break;
+            }
+        }while (choice!=2);
     }
 }
